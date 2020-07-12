4f3aa81a7a2b01d0893354dafbffbc49c978f758
Hammad Ahmad <hammad.ahmad@kbst-gmbh.de>
2020-07-12 08:31:45 +0200

    Added Customers API and MongoDb database

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 const express = require("express");
+const customers = require("./routes/customers");
 const genres = require("./routes/genres");
 const home = require("./routes/home");
 const app = express();
+
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(() => console.log("db connected..."))
+  .catch((err) => console.error("couldn't connect to MongoDb"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +21,7 @@ if (app.get("env") === "development") {
 }
 
 app.use("/api/genres", genres);
+app.use("/api/customers", customers);
 app.use("/", home);
 
 const port = process.env.PORT || 3000;
---
+++ routes/customers.js
@@ -0,0 +1,59 @@
+const { Customer, validate } = require("../models/customer");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const customers = await Customer.find().sort("name");
+
+  if (!customers) res.status(404).send("No Customer Present");
+
+  res.send(customers);
+});
+
+router.get("/:id", async (req, res) => {
+  const customer = await Customer.findById(req.params.id);
+
+  if (!customer) res.status(404).send("Customer Not Found");
+
+  res.send(customer);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) res.status(400).send(error.details[0].message);
+
+  let customer = new Customer({
+    isGold: req.body.isGold,
+    name: req.body.name,
+    phone: req.body.phone,
+  });
+  customer = await customer.save();
+
+  res.send(customer);
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) res.status(400).res.send(error.details[0].message);
+
+  const customer = await Customer.findByIdAndUpdate(
+    req.params.id,
+    { isGold: req.body.isGold, name: req.body.name, phone: req.body.phone },
+    { new: true }
+  );
+
+  if (!customer) res.status(404).res.send("Customer Not Found");
+
+  res.send(customer);
+});
+
+router.delete("/:id", async (req, res) => {
+  const customer = await Customer.findByIdAndRemove(req.params.id);
+
+  if (!customer) res.status(404).send("Customer Not Found");
+
+  res.send(customer);
+});
+
+module.exports = router;
