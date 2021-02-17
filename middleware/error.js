39c7c005ddb429ddee3c97a131b0fcfa577fe5cb
hammaddude1 <hammad.ahmad@outlook.com>
2021-02-18 00:15:24 +0100

    A

--- middleware/error.js
+++
@@ -1,7 +0,0 @@
-const winston = require("winston");
-const { error } = require("winston");
-
-module.exports = function (err, req, res, next) {
-  winston.error(err.message, { metadata: err });
-  res.status(500).send("Unexpected Error Occured");
-};
